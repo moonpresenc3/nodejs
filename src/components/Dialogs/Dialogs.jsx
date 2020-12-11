cefd4b8c9ecc8f2ad7452b8a0cf3e76bae878c12
Чернов Иван Александрович <I.Chernov@zitar.local>
2020-12-11 17:22:06 +0300

    'commitLast11.12'

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 import cl from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-return <div className={cl.content}>
-    Dialogs
-</div>
+    return (
+        <div className={cl.dialogs}>
+            <div className={cl.dialogsItems}>
+                <div className={cl.dialog + ' ' + cl.active}>
+                    Ivan
+                </div>
+                <div className={cl.dialog}>
+                    Dmitry
+                </div>
+                <div className={cl.dialog}>
+                    Stas
+                </div>
+            </div>
+            <div className={cl.messages}>
+                <div className={cl.message}>
+                    Hi
+                </div>
+                <div className={cl.message}>
+                    How
+                </div>
+                <div className={cl.message}>
+                    Are You
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Dialogs;
