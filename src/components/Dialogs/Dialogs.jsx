b0301dda53f23321c26958d5d59607350f1127d1
Чернов Иван Александрович <I.Chernov@zitar.local>
2020-12-14 15:11:42 +0300

    'addComponentsDialogItemAndMessage'

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,5 @@
 import React from "react";
 import cl from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
-
-const DialogItem = (props) => {
-  let path = "/dailogs/" + props.id;
-  return (
-    <div className={cl.dialog + " " + cl.active}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  );
-};
-
-const Message = (props) => {
-  return <div className={cl.dialog}>{props.message}</div>;
-};
 
 const Dialogs = (props) => {
   let dialogsData = [
---
+++ src/components/Dialogs/Message/Message.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import cl from "./../Dialogs.module.css";
+
+const Message = (props) => {
+  return <div className={cl.dialog}>{props.message}</div>;
+};
+
+export default Message;
