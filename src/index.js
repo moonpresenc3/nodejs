6a3b2c018186f7167068ab2b27db1873049d92a1
unknown <kiraoff@yandex.ru>
2020-12-20 18:03:51 +0300

    'home42'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from "./redux/state";
+import store from "./redux/store";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
